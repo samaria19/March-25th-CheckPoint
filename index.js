b4d9608d08a122b2837c06ae9b2aafd5f227faeb
Jonathan Corley <jcorley@noscihigh.org>
2021-03-26 15:56:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/*
+Instructions:
+for loops
+Declare a variable named myArray and assign it to the value of an array literal
+
+Using a for loop, and the push method, push into the array the numbers 1 thru 20;
+For loops (cont...)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Inside the function printer, create a for loop that prints all of the values in 
+myArray in reverse to the console
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+For in loops
+Declare a variable named myObject and assign it the value of an object literal.
+
+
+Using dot notation, add three keys, first, second, and third and assign to each key 
+the value of a 10, 20, and 30
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+Using a for in loop, and the addition assignment(+=) operator, add five to each value in myObject
+*/
