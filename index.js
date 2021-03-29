260d8053416153be9c550e429d5299770aa783e1
Samaria Smothers <ssmothers457@noscihigh.org>
2021-03-29 02:42:06 +0000

    3

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ Using a for loop, and the push method, push into the array the numbers 1 thru 20
 For loops (cont...)
 */
 
+/*
+var myArray = []
 
+for(var i=1;i<=20;i++ ){
+  myArray.push(i)
+}
+console.log(myArray)
 
-
-
+*/
 
 
 
@@ -22,8 +27,13 @@ For loops (cont...)
 Inside the function printer, create a for loop that prints all of the values in 
 myArray in reverse to the console
 */
+/*
+function printer
 
-
+for(var myArray.length i<=0){
+print(myArray)
+}
+*/
 
 
 
@@ -41,15 +51,17 @@ Using dot notation, add three keys, first, second, and third and assign to each 
 the value of a 10, 20, and 30
 */
 
+var myObject = {};
 
-
-
-
-
-
+myObject.first = 10
+myObject.second = 20 
+myObject.third  = 30
 
 
 
 /*
 Using a for in loop, and the addition assignment(+=) operator, add five to each value in myObject
 */
+for(var key in myObject){
+  
+}
